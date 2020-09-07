6b6c3434b2f4975064cbad5de4fec5d29151b438
Aman <30631556+aman16997@users.noreply.github.com>
2020-09-07 09:07:18 +0530

    Content Fixes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 // document.getElementById("hero_image");
+
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
